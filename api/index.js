02ca051dcd27f501a552fb8459a4db9a1d2f8807
Lucas Le Ray <contact@lucas-le-ray.com>
2020-10-03 19:08:16 +0200

    fix: reducing max songs retrieved

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const Client = new Genius.Client(process.env.GENIUS_ACCESS_TOKEN)
 async function retrieveLyrics() {
   const partialSongs = await Client.songs.search("Pop Smoke")
   const artist = await Client.artists.get(partialSongs[0].artist.id)
-  const songs = await artist.songs({ per_page: 50 })
+  const songs = await artist.songs({ per_page: 10 })
   const indexSongs = Math.floor(Math.random() * Math.floor(songs.length))
 
   let lyrics
