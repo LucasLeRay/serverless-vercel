03d8b706d266c97a48c68bab2f5489f9f08e9072
Lucas Le Ray <contact@lucas-le-ray.com>
2020-10-03 18:58:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Serverless Vercel
+
+Simple test Serverless API deployed on Vercel.
+
+More details here : 
+
+## How to use
+
+- Create `.env` file containing `GENIUS_ACCESS_TOKEN` (the access token from Genius API)
+
+- Deploy with Vercel CLI:
+```
+vercel --prod
+```
---
+++ api/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const Genius = require("genius-lyrics")
+
+const Client = new Genius.Client(process.env.GENIUS_ACCESS_TOKEN)
+
+async function retrieveLyrics() {
+  const partialSongs = await Client.songs.search("Pop Smoke")
+  const artist = await Client.artists.get(partialSongs[0].artist.id)
+  const songs = await artist.songs({ per_page: 50 })
+  const indexSongs = Math.floor(Math.random() * Math.floor(songs.length))
+
+  let lyrics
+  while (!lyrics) {
+    try {
+      lyrics = await songs[indexSongs].lyrics(true)
+    } catch (err) { console.error(err) }
+  }
+  const arrLyrics = lyrics.split("\n").filter(l => l.length && l[0] !== '[')
+  const indexLyrics = Math.floor(Math.random() * Math.floor(arrLyrics.length))
+  return arrLyrics[indexLyrics]
+}
+
+module.exports = async (req, res) => {
+  try {
+    res.send({
+      status: 200,
+      lyrics: await retrieveLyrics(),
+    })
+  } catch (err) {
+    res.send({
+      status: 500,
+      message: err.message,
+    })
+  }
+}
